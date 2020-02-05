2a49f2d43fcdc5e08b2739a5a84fe30d556e0da3
Denis Deniben <deniben2109@gmail.com>
2020-02-06 00:27:45 +0200

    added WebSecurityConfig.java with configured method

--- src/main/java/com/windigital/spring/todo/SpringBootTodoAppApplication.java
+++ src/main/java/com/windigital/spring/todo/SpringBootTodoAppApplication.java
@@ -2,8 +2,6 @@ package com.windigital.spring.todo;
 
 import com.windigital.spring.todo.domain.Todo;
 import com.windigital.spring.todo.repository.TodoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
